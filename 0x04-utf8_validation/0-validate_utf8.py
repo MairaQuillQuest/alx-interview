f2d62968fa3dce3fb8a43f941eed26ff923f7b66
MairaQuillQuest <ririsoniah@gmail.com>
2024-07-28 19:52:22 -0700

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,25 +1,39 @@
 #!/usr/bin/python3
 """
-validUTF8
+Write a method that determines if a given data set
+represents a valid UTF-8 encoding.
 """
 
-def validUTF8(data):
-    """Determines if a given data set represents a valid UTF-8 encoding"""
-    count = 0
 
-    for x in data:
-        if count == 0:
-            if (x >> 5) == 0b110:
-                count = 1
-            elif (x >> 4) == 0b1110:
-                count = 2
-            elif (x >> 3) == 0b11110:
-                count = 3
-            elif (x >> 7):
+def validUTF8(data):
+    """Determine if a given dataset represents utf8."""
+    n_bytes, mask1, mask2 = 0, 1 << 7, 1 << 6
+
+    for num in data:
+        mask = 1 << 7
+        if n_bytes == 0:
+            while mask & num:
+                n_bytes += 1
+                mask = mask >> 1
+            if n_bytes == 0:
+                continue
+            if n_bytes == 1 or n_bytes > 4:
                 return False
         else:
-            if (x >> 6) != 0b10:
+            if not (num & mask1 and not (num & mask2)):
                 return False
-            count -= 1
+        n_bytes -= 1
+    return n_bytes == 0
 
-    return count == 0
+
+if __name__ == "__main__":
+    data = [65]
+    print(validUTF8(data))
+
+    data = [80, 121, 116, 104, 111, 110, 32, 105,
+            115, 32, 99, 111, 111, 108, 33
+            ]
+    print(validUTF8(data))
+
+    data = [229, 65, 127, 256]
+    print(validUTF8(data))
